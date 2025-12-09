029d3b8b1198621621c58c96e9892b376c4e7522
SK Mumitur Rahman <sk11mumit@gmail.com>
2025-12-09 14:37:29 +0600

    Update and Complete task.php

--- LabTask_PHP/task.php
+++ LabTask_PHP/task.php
@@ -108,13 +108,14 @@ function printShapes() {
 <?php
 //8
 //print shapes from a 2D array
+
 function print2DArrayShapes() {
     $array = [
         [1, 2, 3, 'A'],
         [1, 2, 'B', 'C'],
         [1, 'D', 'E', 'F']
     ];
-
+/*
     for ($i = 0; $i < count($array); $i++) {
         for ($j = 0; $j < count($array[$i]); $j++) {
             if(is_numeric($array[$i][$j])){
@@ -134,8 +135,24 @@ function print2DArrayShapes() {
         echo "<br>";
     }
 }
-
-
+*/
+    for ($i = 0; $i < count($array); $i++) {
+        
+        for ($j = 0; $j < count($array[$i]); $j++) {
+            if(is_numeric($array[$i][$j])){
+                echo $array[$i][$j];
+            }  
+            
+            if(is_string($array[$i][$j])){
+               
+               echo "  ".$array[$i][$j];
+            }     
+        }
+        //echo"_"; 
+        //echo "";
+        echo "<br>";
+    }
+}
 ?>
 
 <?php
